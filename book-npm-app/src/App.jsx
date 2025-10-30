92a2211fa29688b9597d03ea6a13fc6a2d388fef
Abhinav Kumar <kumabhi139@gmail.com>
2025-10-30 11:35:12 -0400

    Updated Book component with Cart alert and centered layout

--- book-npm-app/src/App.jsx
+++ book-npm-app/src/App.jsx
@@ -6,9 +6,15 @@ import Book from './components/Book'
 
 function App() {
   const bookjson=[
-    {image:"https://th.bing.com/th/id/OIP.a3JiRaDCCev5-0HMATz4QgHaIz?w=195&h=232&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", title:"Physics", price:451},
-    {image:"https://th.bing.com/th/id/OIP.vcQQNyGz3qe7Ee0WS9KVWAHaI4?w=157&h=188&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", title:"Math", price:781},
-    {image:"https://th.bing.com/th/id/OIP.4sH-8jMvjVtMgfijC4mzCgHaJd?w=195&h=249&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", title:"Chemistry", price:589}
+    {image:"https://th.bing.com/th/id/OIP.a3JiRaDCCev5-0HMATz4QgHaIz?w=195&h=232&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", 
+     title:"Physics", 
+     price:451},
+    {image:"https://th.bing.com/th/id/OIP.vcQQNyGz3qe7Ee0WS9KVWAHaI4?w=157&h=188&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", 
+     title:"Math", 
+     price:781},
+    {image:"https://th.bing.com/th/id/OIP.4sH-8jMvjVtMgfijC4mzCgHaJd?w=195&h=249&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3", 
+     title:"Chemistry", 
+     price:589}
   ]
   return (
       <div className='bookList'>
--- book-npm-app/src/components/Book.jsx
+++ book-npm-app/src/components/Book.jsx
@@ -3,11 +3,20 @@ import React from 'react'
 import '../App.css';
 
 function Book({book}){
+
+  const handleClick = ()=>{
+    alert("Added to Cart");
+  }
+
   return (
     <div className='card'>
       <img src={book.image} alt="book IMG" style={{height:"100px", width:"100px"}}/>
       <h3>Title : {book.title}</h3>
       <h4>Price : ₹{book.price}/-</h4>
+      <div style={{display:"flex", gap:"10px"}}>
+        <button onClick={handleClick}>Cart</button>
+        <button>Buy</button>
+      </div>
     </div>
   );
 }
